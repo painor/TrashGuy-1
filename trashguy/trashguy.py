4558bc1a62b98580a286f916e336e7ec2b174ab1
painor <30215770+painor@users.noreply.github.com>
2019-12-17 21:54:34 +0100

    Remove uselss stuff

--- trashguy/trashguy.py
+++ trashguy/trashguy.py
@@ -1,21 +1,3 @@
-# ============================================= #
-#             Trash Guy Script                  #
-#                 (> ^_^)>                      #
-#          Made by Zac (t.me/Zacci)             #
-#                                               #
-#      Donate:                                  #
-#      1CoRm4mKCUPs5XQnFVSVQ4xGMAp29pyYzC       #
-# ============================================= # =========================== #
-# Copyright (C) 2019 Zac (https://t.me/Zacci)                                 #
-# Permission is hereby granted, free of charge, to any person obtaining a     #
-# copy of this software and associated documentation files (the "Software"),  #
-# to deal in the Software without restriction, including without limitation   #
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,    #
-# and/or sell copies of the Software, and to permit persons to whom the       #
-# Software is furnished to do so, subject to the following conditions: The    #
-# above copyright notice and this permission notice shall be included in all  #
-# copies or substantial portions of the Software.                             #
-# =========================================================================== #
 from typing import Union, Iterator, Tuple
 from ._types import SpriteVars, SliceVars
 from ._frame_engine import FrameEngine
